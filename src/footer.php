95ec8cead7fcefcdf5895ca09526c18ff1d523c7
franciskinyuru <franciskinyuru26@gmail.com>
2020-06-07 09:16:55 +0300

    added footer

---
+++ src/footer.php
@@ -0,0 +1,66 @@
+<script src="Assets/js/jquery-3.5.0.js" integrity="sha256-MlusDLJIP1GRgLrOflUQtshP0TwT/RHXsl1wWGnQhs=" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="{{asset('Assets/js/jquery.min.js')}}"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="Assets/css/bootstrap.min.css">
+    <link href="Assets/css/styles.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" type="text/css" href="{{asset('Assets/css/bootstrap.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('Assets/css/bootstrap.min.css')}}">
+<script type="text/javascript" src="{{asset('Assets/js/jquery.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('Assets/js/bootstrap.bundle.js')}}"></script>
+<script type="text/javascript" src="{{asset('Assets/js/bootstrap.bundle.min.js')}}"></script>
+
+
+
+<div class="partn"><br>
+    <div class="contain">
+    <center>
+        <h3>OUR PARTNERS</h3>
+    </center>
+    <div class="gallery">
+        <img src="Assets/icons/facebook.png" alt="facebook">
+        <a href="#"><div class="descr">FACEBOOK</div></a>
+    </div>
+    <div class="gallery">
+        <img src="Assets/icons/andela.png" alt="andela">
+        <a href="#"><div class="descr">ANDELA</div></a>
+    </div>
+    <div class="gallery">
+        <img src="Assets/icons/un.png" alt="united">
+        <a href="#"><div class="descr">UNITED NATIONS</div></a>
+    </div>
+    <hr style="color: solid #e2af6d; border-top: 3px solid #e2af6d;">
+</div>
+</div>
+<div class="lower">
+    <div class="contain">
+        <div class="colu">
+        <div class="row">
+            <div class="column">
+            <a href="#call"><img src="Assets/logos/pne.png"><b><p>+254701255700</b></p></a>
+            
+            <a href="#email"><img src="Assets/logos/email.png"><p><b>info@fundisha.co.ke</b></p></a>
+            
+            <a href="#location"><img src="Assets/logos/loca.png"><p><b>1st floor Avenue Towers Nairobi</b></p></a>
+            
+            </div>
+            <div class="column">  
+                <a href="jobs"><p><b>Jobs</b></p></a>
+                <a href="morecourses"><p><b>Courses</b></p></a>
+                <a href=""><p><b>About Us</b></p></a>
+                <a href=""><p><b>Contact Us</b></p></a>
+            </div>
+            <div class="column">
+                <a href=""><p><b>Blogs</b></p></a>
+                <a href=""><p><b>FAQs</b></p></a>
+                <a href=""><p><b>Privacy Policy</b></p></a>
+                <a href=""><p><b>Terms & Conditions</b></p></a>
+            </div>
+        </div>
+        </div>
+   <hr style="color: solid #e2af6d; border-top: 3px solid #e2af6d;"> 
+</div>
+<div class="footer">
+    <a href="#"><h4>FUNDISHA</h4></a>
+    <p>&copy 2020. All rights reserved</p>
+</div>
+</div>
